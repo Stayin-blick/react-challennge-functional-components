198b8ed12421326564cdc81f60cdc703f72fcc5e
Stayin_blick <azeezbello75@gmail.com>
2023-07-08 15:30:22 +0000

    Lifecycles Challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,20 +2,36 @@ import React, { Component } from "react";
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 export class Content extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        isLoaded: false
+      }
+    }
+
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
+    }
 
     render() {
         return (
             <div className={css.Content}>
-                
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
                 </div>
-
                 <div className={css.SearchResults}>
-
-                    {/* Part 1: Creating the map function */}
+                    {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={savedPosts} />
+                        : <Loader />
+                    }
 
                     {
                     savedPosts.map((post)=>{
@@ -27,9 +43,6 @@ export class Content extends Component {
                                 </div>
                         })
                     }
-
-
-                    {/* Part 2: Creating a child component */}
 
                     <PostItem savedPosts={savedPosts} />
                 </div>
